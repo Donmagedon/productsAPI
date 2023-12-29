d98bb7125211fa59334e310f813020dea8a892d3
donmagedon <donmagedon7@gmail.com>
2023-12-29 14:37:12 -0500

    testing server

--- routes/items.js
+++ routes/items.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const router = express.Router()
 router.use("/",(req,res)=>{
-    res.status(200,"hi there,this is items!")
+    res.send("hi this is items")
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
-const connectDB = require("./mongoDB")
 const items = require("./routes/items")
-const secret = require("dotenv").config()
+app.use("",(req,res)=>{
+  res.send("hi I'm alive!")
+})
 app.use("/items",items)
 app.listen(3330,()=>{
   console.log("server listening!");  
