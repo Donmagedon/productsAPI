8e12fd507ca81ddfa38a57576a58591fc0a6a3d7
donmagedon <donmagedon7@gmail.com>
2023-12-29 20:31:49 -0500

    change port setting

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const items = require("./routes/items")
+const port = 3330
 app.use("",(req,res)=>{
   res.send("hi I'm alive!")
 })
 app.use("/items",items)
-app.listen(3330,()=>{
+app.listen(port,"0.0.0.0",()=>{
   console.log("server listening!");  
 })
 
